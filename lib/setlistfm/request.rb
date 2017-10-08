906e109bf607b9e225d47bddc77573ee5d53bd21
Hondallica <hondallica@gmail.com>
2017-10-08 10:26:28 +0900

    add http client

--- lib/setlistfm/request.rb
+++ lib/setlistfm/request.rb
@@ -1,6 +1,19 @@
+require 'faraday'
+require 'faraday_middleware'
+
 module Request
   def base_url(url = 'https://api.setlist.fm')
     url
+  end
+
+  def http
+    Faraday.new(url: base_url) do |f|
+      f.request :url_encoded
+      f.headers['x-api-key'] = @api_key
+      f.headers['Accept'] = 'application/json'
+      f.headers['User-Agent'] = 'ruby-setlistfm'
+      f.adapter :net_http
+    end
   end
 
   module_function :base_url
---
+++ spec/request_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe Setlistfm do
+  context 'Request' do
+    it 'has a HTTP client' do
+      setlistfm = Setlistfm.new('your_api_key')
+      expect(setlistfm.http.class).to eq Faraday::Connection
+    end
+
+    it 'has a x-api-key header' do
+      setlistfm = Setlistfm.new
+      expect(setlistfm.http.headers['x-api-key']).to be_truthy
+    end
+
+    it 'has a User-Agent' do
+      setlistfm = Setlistfm.new
+      expect(setlistfm.http.headers['User-Agent']).to eq 'ruby-setlistfm'
+    end
+
+    it 'has an Accept header ' do
+      setlistfm = Setlistfm.new
+      expect(setlistfm.http.headers['Accept']).to eq 'application/json'
+    end
+  end
+end
