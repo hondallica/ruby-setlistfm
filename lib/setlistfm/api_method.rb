7adb0f8963701a4b2607b36576c7232ed84b9210
Hondallica <hondallica@gmail.com>
2017-10-08 18:27:14 +0900

    add artist_setlists method

--- lib/setlistfm/api_method.rb
+++ lib/setlistfm/api_method.rb
@@ -2,4 +2,8 @@ module APIMethod
   def artist(mbid)
     http.get("/rest/#{api_version}/artist/#{mbid}")
   end
+
+  def artist_setlists(mbid, params = {})
+    http.get("/rest/#{api_version}/artist/#{mbid}/setlists", params)
+  end
 end
