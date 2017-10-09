465c3bf7bd104edce37f41bff9051dc87c228bbd
Hondallica <hondallica@gmail.com>
2017-10-09 22:38:39 +0900

    add venue_setlists method

--- spec/api_method/venue_setlists_spec.rb
+++ spec/api_method/venue_setlists_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe Setlistfm do
   end
   
   let(:setlistfm) { Setlistfm.new('your_api_key') }
-  let(:url) { %r|https://api\.setlist\.fm/rest/1\.0/venue/.+\?*| }
+  let(:url) { %r|https://api\.setlist\.fm/rest/1\.0/venue/.+/setlists\?*| }
 
-  context 'venue' do
-    let(:fixture) { 'spec/fixtures/venue/200.json' }
-    it 'venue' do
-      res = setlistfm.venue('6bd6ca6e')
+  context 'venue_setlists' do
+    let(:fixture) { 'spec/fixtures/venue/setlists/200.json' }
+    it 'venue_setlists' do
+      res = setlistfm.venue_setlists('6bd6ca6e')
       expect(res.status).to eq 200
     end
   end
