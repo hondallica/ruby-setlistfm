1f67e14dbcef97d5b6a279e4cf2e0edff95f2cb4
Hondallica <hondallica@gmail.com>
2017-10-09 21:28:08 +0900

    add search_setlists method

---
+++ spec/api_method/search_setlists_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Setlistfm do
+  before do
+    WebMock.stub_request(:get, url).to_return(
+      body: File.read(fixture),
+      status: 200,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+  
+  let(:setlistfm) { Setlistfm.new('your_api_key') }
+  let(:url) { %r|https://api\.setlist\.fm/rest/1\.0/search/setlists\?*| }
+  let(:mbid) { '27e2997f-f7a1-4353-bcc4-57b9274fa9a4' }
+
+  context 'search_setlists' do
+    let(:fixture) { 'spec/fixtures/search/setlists/200.json' }
+
+    it 'artistMbid' do
+      res = setlistfm.search_setlists({artistMbid: mbid})
+      expect(res.status).to eq 200
+    end
+  end
+end
