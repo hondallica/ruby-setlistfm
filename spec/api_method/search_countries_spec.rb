64a640e12339eca98df8dd86e8093caf4fa639e4
Hondallica <hondallica@gmail.com>
2017-10-09 21:15:03 +0900

    add search_countries method

---
+++ spec/api_method/search_countries_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Setlistfm do
+  before do
+    WebMock.stub_request(:get, url).to_return(
+      body: File.read(fixture),
+      status: 200,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+  
+  let(:setlistfm) { Setlistfm.new('your_api_key') }
+  let(:setlistfm) { Setlistfm.new() }
+  let(:url) { %r|https://api\.setlist\.fm/rest/1\.0/search/countries\?*| }
+
+  context 'search_countries' do
+    let(:fixture) { 'spec/fixtures/search/countries/200.json' }
+    it 'countries' do
+      res = setlistfm.search_countries
+      expect(res.status).to eq 200
+      expect(res.body.page).to eq 1
+    end
+  end
+end
